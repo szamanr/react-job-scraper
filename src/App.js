bd673c3f3519ab64c3198a1a3e2139de9c3751e6
rrrafalsz <rrrafalsz@gmail.com>
2021-01-26 15:18:27 +0100

    tidy up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
 import React from "react";
 import JobList from "./JobList";
-import {BrowserRouter, Route} from "react-router-dom";
-import JobDetails from "./JobDetails";
 
 function App() {
     return (
-        <BrowserRouter>
-            <main className="App">
-                <Route exact path="/">
-                    <JobList/>
-                </Route>
-
-                <Route path="/job/:id">
-                    <JobDetails/>
-                </Route>
-            </main>
-        </BrowserRouter>
+        <main className="App">
+            <JobList/>
+        </main>
     );
 }
 
